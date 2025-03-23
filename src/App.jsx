fa6adf8831f22e79df14ed5f4e384a22de58b286
Samaa <samaaalaa12005@gmail.com>
2025-03-23 19:45:11 +0200

    edit upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className=" min-h-screen flex flex-col items-center justify-center text-white px-6">
+    <div className=" md:h-[100vh] h-[150vh]  flex flex-col items-center justify-center text-white px-6 py-4">
       <div className="flex flex-col items-center text-center space-y-2 mt-6 animate-fade-in mb-12">
         <div className="flex items-center space-x-2">
           <FileText className="text-blue-400 drop-shadow-glow animate-bounce" size={42} />
-          <h2 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
+          <h2 className="md:text-4xl text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
             OCR Text Extractor
           </h2>
         </div>
